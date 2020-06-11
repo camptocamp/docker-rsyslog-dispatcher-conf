687a5eeda3bd481f97760248d67785ca651298c1
Marc Fournier <marc.fournier@camptocamp.com>
2020-06-11 13:49:28 +0200

    copy omrelp template and adapt it for omkafka to allow a smooth transition to kafka direct connection

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,9 @@
 FROM camptocamp/rsyslog-bin:v0.1.11
 
-ADD ./conf.d /etc/confd/conf.d
-ADD ./templates /etc/confd/templates
+ADD ./confd/rsyslog-kafka.conf.toml /etc/confd/kafka/conf.d/
+ADD ./templates/rsyslog-kafka.conf.tmpl /etc/confd/kafka/templates/
+
+ADD ./confd/rsyslog-relp.conf.toml /etc/confd/relp/conf.d/
+ADD ./templates/rsyslog-relp.conf.tmpl /etc/confd/relp/templates/
 
 COPY /docker-entrypoint.d/* /docker-entrypoint.d/
--- docker-entrypoint.d/05-run-confd.sh
+++ docker-entrypoint.d/05-run-confd.sh
@@ -4,5 +4,11 @@ mkdir -p /etc/rsyslog-confd
 
 while true; do
   sleep 1
-  confd --backend env --onetime && break
+  if [ -z "$KAFKA_TOPIC" ]; then
+    echo "relp-based config, with settings: TARGET: '$TARGET', TOKEN: '$TOKEN'"
+    confd -backend env -confdir /etc/confd/relp/ -onetime && break
+  else
+    echo "kafka-based config, with settings: KAFKA_TOPIC: '$KAFKA_TOPIC', KAFKA_BROKERS: '$KAFKA_BROKERS', KAFKA_PARAMS: '$KAFKA_PARAMS'"
+    confd -backend env -confdir /etc/confd/kafka/ -onetime && break
+  fi
 done
