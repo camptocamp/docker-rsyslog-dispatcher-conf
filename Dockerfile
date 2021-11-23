6f404bbe9f14a6167e89e54efdbe42ef43ed33c3
Lenny Consuegra <lenny.consuegra@camptocamp.com>
2021-11-23 11:04:22 +0100

    chore: add relp and kafka compatible config

--- Dockerfile
+++ Dockerfile
@@ -6,4 +6,7 @@ ADD ./templates/rsyslog-kafka.conf.tmpl /etc/confd/kafka/templates/
 ADD ./confd/rsyslog-relp.conf.toml /etc/confd/relp/conf.d/
 ADD ./templates/rsyslog-relp.conf.tmpl /etc/confd/relp/templates/
 
+ADD ./confd/rsyslog-relp-kafka.conf.toml /etc/confd/relpkafka/conf.d/
+ADD ./templates/rsyslog-relp-kafka.conf.tmpl /etc/confd/relpkafka/templates/
+
 COPY /docker-entrypoint.d/* /docker-entrypoint.d/
--- docker-entrypoint.d/05-run-confd.sh
+++ docker-entrypoint.d/05-run-confd.sh
@@ -7,8 +7,11 @@ while true; do
   if [ -z "$KAFKA_TOPIC" ]; then
     echo "relp-based config, with settings: TARGET: '$TARGET', TOKEN: '$TOKEN'"
     confd -backend env -confdir /etc/confd/relp/ -onetime && break
-  else
+  elif [ -z "$TARGET" ]; then
     echo "kafka-based config, with settings: KAFKA_TOPIC: '$KAFKA_TOPIC', KAFKA_BROKERS: '$KAFKA_BROKERS', KAFKA_PARAMS: '$KAFKA_PARAMS'"
     confd -backend env -confdir /etc/confd/kafka/ -onetime && break
+  else
+    echo "relp-kafka-based config, with settings: TARGET: '$TARGET', TOKEN: '$TOKEN', KAFKA_TOPIC: '$KAFKA_TOPIC', KAFKA_BROKERS: '$KAFKA_BROKERS', KAFKA_PARAMS: '$KAFKA_PARAMS'"
+    confd -backend env -confdir /etc/confd/relpkafka/ -onetime && break
   fi
 done
